1b89c2bc8efc7b335f53be1b50c00b17e797d920
Steve Ranford <steve.ranford@warwick.ac.uk>
2016-04-13 16:29:44 +0100

    incorporate comments into short item style

--- neatline/exhibits/themes/common/item-short.php
+++ neatline/exhibits/themes/common/item-short.php
@@ -15,8 +15,13 @@
 <?php echo link_to(
   get_current_record('item'), 'show', 'More detail on this clip', ['class' => 'btn btn-warning btn-block']
 ); ?>
-</div>
-<?php CommentingPlugin::showComments(); ?>
+
+<?php CommentingPlugin::showComments(); 
+
+?>
+
+<?php echo '<a href="/items/show/' . $item['id'] .'#comments" class="btn btn-primary btn-block" target="_blank">Add a comment</a>'; ?>
+
 <script>
 Neatline.module('Presenter.StaticBubble', function(StaticBubble) {
 

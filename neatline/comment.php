22cff3b6e220264bf521f6365d44538b9087065c
Steve Ranford <steve.ranford@warwick.ac.uk>
2016-04-13 17:24:16 +0100

    fixed the uneven div tags

--- neatline/comment.php
+++ neatline/comment.php
@@ -10,20 +10,21 @@
         }
     ?>
     <p class='comment-author-name media-left'>
-    <?php
-        $hash = md5(strtolower(trim($comment->author_email)));
-        $url = "//www.gravatar.com/avatar/$hash";
-        echo "<img class='media-object img-circle' src='$url' />";
-    ?>
-    <?php echo $authorText?></p>
+        <?php
+            $hash = md5(strtolower(trim($comment->author_email)));
+            $url = "//www.gravatar.com/avatar/$hash";
+            echo "<img class='media-object img-circle' src='$url' />";
+        ?>
+        <?php echo $authorText?>
+    </p>
   <div class="media-body">
-  <div>
-    <?php echo $comment->body; ?>
-  </div>
-<?php if(is_allowed('Commenting_Comment', 'unflag')): ?>
-    <div class="comment-buttons-bar">
-        <p class='comment-flag btn btn-xs btn-warning' <?php if($comment->flagged): ?> style='display:none;'<?php endif;?> ><em class="glyphicon glyphicon-flag"></em> <?php echo __("Flag inappropriate"); ?></p>
-        <p class='comment-unflag btn btn-xs btn-success' <?php if(!$comment->flagged): ?>style='display:none;'<?php endif;?> ><em class="glyphicon glyphicon-flag"></em> <?php echo __("Unflag inappropriate"); ?></p>
+      <div>
+        <?php echo $comment->body; ?>
+      </div>
+        <div class="comment-buttons-bar">
+        <?php if(is_allowed('Commenting_Comment', 'unflag')): ?>
+            <p class='comment-flag btn btn-xs btn-warning' <?php if($comment->flagged): ?> style='display:none;'<?php endif;?> ><em class="glyphicon glyphicon-flag"></em> <?php echo __("Flag inappropriate"); ?></p>
+            <p class='comment-unflag btn btn-xs btn-success' <?php if(!$comment->flagged): ?>style='display:none;'<?php endif;?> ><em class="glyphicon glyphicon-flag"></em> <?php echo __("Unflag inappropriate"); ?></p>
         <?php endif; ?>
         <p class='comment-reply btn btn-xs btn-primary'><em class="glyphicon glyphicon-share-alt"></em> <?php echo __("Reply"); ?></p>
     </div>

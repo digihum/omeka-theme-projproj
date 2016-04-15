0fe9025151333a8e6c3c0aae8ddfc682236a62cf
Steve Ranford <steve.ranford@warwick.ac.uk>
2016-04-15 10:09:32 +0100

    simplify comments

--- neatline/exhibits/themes/common/item-comments.php
+++ neatline/exhibits/themes/common/item-comments.php
@@ -1,21 +1,3 @@
-<?php // Tag a neatline record with 'custom' to show the body of this file in that record's presenter ?>
-<?php if (metadata('item',array('Dublin Core','Description'))): ?>
-    <div id="description" class="element">
-        <p><?php echo metadata('item',array('Dublin Core','Description')); ?></p>
-    </div>
-<?php endif; ?>
-
-<?php if (metadata('item', 'has files')): ?>
-  <h3><?php echo __('Clip'); ?></h3>
-  <?php echo files_for_item(); ?>
-<?php endif; ?>
-
-<?php echo get_specific_plugin_hook_output('ItemRelations','public_items_show', array('view' => $this, 'item' => $item)); ?>    
-<!-- Link. -->
-<div class="more-link">
-<?php echo link_to(
-  get_current_record('item'), 'show', 'More detail on this clip', ['class' => 'btn btn-warning btn-block']
-); ?>
-
 <?php CommentingPlugin::showComments(); ?>
-</div>
+
+<?php echo '<a href="/items/show/' . $item['id'] .'#comments" class="btn btn-primary btn-block" target="_blank">Add a comment</a>'; ?>

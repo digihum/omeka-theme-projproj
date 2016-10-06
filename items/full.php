dc583d4f7a5003e9560e2b644adf89031ea4e72f
Steve Ranford <steve.ranford@warwick.ac.uk>
2016-10-06 16:44:57 +0100

    new partial views for news and events

---
+++ items/full.php
@@ -0,0 +1,46 @@
+<div class="item record">
+    <?php
+    $title = metadata($item, array('Dublin Core', 'Title'));
+    $description = metadata($item, array('Dublin Core', 'Description'));
+    $text = metadata($item, array('Item Type Metadata', 'Text'));
+    $tweet = metadata($item, array('Item Type Metadata', 'Embed Code'), array('no_escape' => true));
+    ?>
+
+
+    <?php if ($title!="[Untitled]"): ?>
+        <h4><?php echo strip_formatting(metadata($item, array('Dublin Core', 'Title'))) ?></h4>
+    <?php endif; ?>
+
+    <small>Published <?php echo  metadata($item, array('Dublin Core', 'Date')) ?></small>
+
+    <div class="row">
+        <div class="col-xs-12 col-md-8">
+
+            <?php if ($text = metadata($item, array('Item Type Metadata', 'Text'))): ?>
+                <div class="item-description">
+                    <p><?php echo $text; ?></p>
+                </div>
+            <?php endif; ?>
+            <?php if ($tweet): ?>
+                <p class="item-tweet"><?php echo $tweet; ?></p>
+            <?php endif; ?>
+            
+        </div>
+   
+
+  <?php if (metadata($item, 'has files')): ?>
+    <div class="col-xs-12 col-md-4">
+        <?php
+            echo link_to_item(
+                item_image('square_thumbnail', array(), 0, $item), 
+                array('class' => 'image'), 'show', $item
+                ); 
+        ?>
+    </div>
+  <?php endif; ?>
+
+              
+    </div>
+  
+</div>
+   <hr>

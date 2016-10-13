f198c55f0a84d0bf10eaddb8b19ee74ebaba22b0
Steve Ranford <Steve.Ranford@warwick.ac.uk>
2016-10-13 10:45:23 +0100

    more info in footer

--- common/footer.php
+++ common/footer.php
@@ -4,8 +4,8 @@
         <hr>
         <div>
             <p class="text-center">
-                <?php echo __('Copyright &copy; ') . date('Y') . ' ' . link_to_home_page() . ', All Rights Reserved.'; ?><br>
-                <?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?>
+                <?php echo __('Copyright &copy; ') . date('Y') . ' ' . link_to_home_page() . ', University of Warwick, All Rights Reserved. | <a href="/terms">Terms</a> | <a href="cookies">Cookies</a>'; ?><br>
+                <?php echo __('Powered by <a href="http://omeka.org">Omeka</a>.'); ?>
             </p>
         </div>
         <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>

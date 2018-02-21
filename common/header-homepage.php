edddf1c56106351ddfd20cd131dab54eb1e6490e
Tim Hollies <tim.hollies@warwick.ac.uk>
2018-02-21 14:17:24 +0000

    Homepage column

--- common/header-homepage.php
+++ common/header-homepage.php
@@ -18,8 +18,8 @@
 <div class="hero-image slide-2"></div>
 <div class="hero-image slide-3"></div>
 <div class="hero-image slide-4"></div>
-<section class="archive">
-</section>
+
 <main id="content" role="main">
-  <div class="container">
-      <?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
+    <div class="hero-image-fadeout"></div>
+    <div class="container">
+    <?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>

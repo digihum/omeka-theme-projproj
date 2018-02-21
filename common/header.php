b020c030cbcc37afbb87cc75a9d442f6eff4be3f
Tim Hollies <tim.hollies@warwick.ac.uk>
2018-02-21 11:40:17 +0000

    Added header switch and style framework

--- common/header.php
+++ common/header.php
@@ -22,96 +22,28 @@
 
     <!-- Need to add custom and third-party CSS files? Include them here -->
     <?php
-        queue_css_file('lib/bootstrap.min');
-        queue_css_file('bootstrap-custom');
-        queue_css_file('style');
+        queue_css_file('site');
         echo head_css();
     ?>
 
-    <!-- Need more JavaScript files? Include them here -->
-    <link rel="stylesheet/less" href="/themes/projproj/css/bootstrap-custom.less" type="text/css" />
     <?php
         queue_js_file('lib/bootstrap.min');
         queue_js_file('globals');
         echo head_js();
     ?>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.3/less.js"></script>
-    <!--[if lt IE 9]>
+  
+      <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<?php 
 
-      ga('create', 'UA-37692171-26', 'auto');
-      ga('send', 'pageview');
+echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass));
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-    </script>
-    <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
-    <header role="banner" class="container">
-    <div id="masthead" class="transparent">
-          <div class="access-info" style="position: absolute; left: -9999em;">
-                <a href="#main-content" accesskey="c" title="Skip to content [c]">Skip to content</a>
-                <a href="#navigation" accesskey="n" title="Skip to navigation [n]">Skip to navigation</a>
-            </div>
-            <div id="warwick-logo-container" class="on-hover">
-              <a id="warwick-logo-link" href="http://www2.warwick.ac.uk" title="University of Warwick home page"></a>
-                  <div id="warwick-site-links" style="display: inline-block; opacity: 0;">
-                    <ul>
-                      <li><a href="http://www2.warwick.ac.uk/study">Study</a></li>
-                      <li class="spacer">|</li>
-                      <li><a href="http://www2.warwick.ac.uk/research">Research</a></li>
-                      <li class="spacer">|</li>
-                      <li><a href="http://www2.warwick.ac.uk/business">Business</a></li>
-                      <li class="spacer">|</li>
-                      <li><a href="http://www2.warwick.ac.uk/alumni">Alumni</a></li>
-                      <li class="spacer">|</li>
-                      <li><a href="http://www2.warwick.ac.uk/newsandevents">News</a></li>
-                      <li class="spacer">|</li>
-                      <li><a href="http://www2.warwick.ac.uk/about">About</a></li>
-                    </ul>
-                  </div>
-                
-            </div>
-        <div id="utility-container"> 
-            <div id="search-container">
-                <form action="//search.warwick.ac.uk/website"><input type="hidden" name="urlPrefix" value="http://www2.warwick.ac.uk/fac/arts/film/research/current/theprojectionproject/">
-                   <input autocomplete="off" id="search-box" class="large" placeholder="Search Projection Project" name="q"><input alt="Search" id="search-button" type="image" src="/themes/projproj/images/search-images.png" title="Click here to search">
-                </form>
-              </div>
-            </div>
-        </div>
-        <div class="row" id="header">
-            <div class="col-xs-12">
-                <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
-                <h1 class="site-title"><?php echo link_to_home_page(theme_logo()); ?></h1>
-            </div>
-        </div>
-        <div class="row">
-	  
-          <nav class="navbar navbar-default" role="navigation">
-  
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-navigation">
-                        <span class="sr-only">Menu</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-
-                <div class="collapse navbar-collapse" id="primary-navigation">
-                    <?php echo public_nav_main_bootstrap(); ?>
-
-                </div>
-           </nav> 
-         
-    </header>
-    <main id="content" role="main">
-      <div class="container">
-          <?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
+if ($path === '' || $path === '/') {
+    include_once('./header-homepage.php');
+} else {
+    include_once('./header-subpage.php');
+}

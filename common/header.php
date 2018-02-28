082a0c43ca347f873a4b728b6aa955f688cc9347
Tim Hollies <tim.hollies@warwick.ac.uk>
2018-02-28 11:25:26 +0000

    Responsive styling

--- common/header.php
+++ common/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#ffff00" />
     <?php if ( $description = option('description')): ?>
         <meta name="description" content="<?php echo $description; ?>" />
     <?php endif; ?>

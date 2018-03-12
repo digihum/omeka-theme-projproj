dce87e374ef382d128129008256bd6805334e5d9
Tim Hollies <tim.hollies@warwick.ac.uk>
2018-03-12 10:03:52 +0000

    Updated footer

--- common/footer.php
+++ common/footer.php
@@ -1,11 +1,49 @@
       </div>
     </main>
     <footer role="contentinfo" class="container footer ">
+        <div>
+            <div>
+                <h4>About</h4>
+                <ul>
+                    <li>About the Project</li>
+                    <li>Advisory Board</li>
+                    <li>Project Team</li>
+                    <li>Project Partners</li>
+                </ul>
+            </div>
 
+            <div>
+                <h4>The Collections</h4>
+                <ul>
+                    <li>Browse Themes</li>
+                    <li>Virtual Projection Box</li>
+                    <li>The Projectionists Gallery</li>
+                </ul>
+            </div>
 
+            <div>
+                <h4>Current Activity</h4>
+                <ul>
+                    <li>News & Events</li>
+                    <li>Contact Us</li>
+                    <li>Twitter</li>
+                </ul>
+            </div>
 
+            <div>
+                <h4>Partners</h4>
+                <img src="/theme/projproj/images/partners/ahrc_square.png" />
+                <img src="/theme/projproj/images/partners/bfi_square.png" />
+                <img src="/theme/projproj/images/partners/flatpack_square.png" />
+            </div>
+
+        </div>
+        <div>
+            <span>Copyright © 2018 Projection Project , University of Warwick, All Rights Reserved. </span>
+            <span>Terms</span>
+            <span>Cookies</span>
         <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
-
+        </div>
     </footer>
  
 </body>

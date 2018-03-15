70581d8bbc5e6c4ddecf10d302353f5b3ba4e08f
Tim Hollies <tim.hollies@warwick.ac.uk>
2018-03-15 16:56:56 +0000

    Item pages

--- items/show.php
+++ items/show.php
@@ -11,6 +11,13 @@ else :
 
 ?>
     <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+     
+    <?php if (metadata('item', 'Collection Name')): ?>
+        <div id="collection" class="element">
+            <div class="element-text"><p><em>Part of:</em> <?php echo link_to_collection_for_item(); ?></p></div>
+        </div>
+    <?php endif; ?>
+
     <?php if (metadata('item', 'has tags')): ?>
         <div id="item-tags" class="element">
             <div class="element-text"><?php echo tag_string('item'); ?></div>
@@ -50,41 +57,23 @@ else :
     <div class="element-texts">
         <?php echo all_element_texts('item'); ?>
     </div>                
-
-                    
-    <!-- If the item belongs to a collection, the following creates a link to that collection. -->
-    <?php if (metadata('item', 'Collection Name')): ?>
-        <div id="collection" class="element">
-            <h3><?php echo __('Collection'); ?></h3>
-            <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+                 
+    <div class="outputs">
+        <div id="item-citation" class="element">
+            <h3><?php echo __('Citation'); ?></h3>
+            <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
         </div>
-    <?php endif; ?>
-    
-
-    
-    <!-- The following prints a citation for this item. -->
-    <div id="item-citation" class="element">
-        <h3><?php echo __('Citation'); ?></h3>
-        <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
-    </div>
-    
-    <div id="item-output-formats" class="element">
-        <h3><?php echo __('Output Formats'); ?></h3>
-        <div class="element-text"><?php echo output_format_list(); ?></div>
+        
+        <div id="item-output-formats" class="element">
+            <h3><?php echo __('Output Formats'); ?></h3>
+            <div class="element-text"><?php echo output_format_list(); ?></div>
+        </div>
     </div>
               
-	<?php CommentingPlugin::showComments(); ?>
+	<!-- <?php CommentingPlugin::showComments(); ?> -->
 
 
     <?php echo get_specific_plugin_hook_output('ItemRelations','public_items_show', array('view' => $this, 'item' => $item)); ?>    
-
-  
-    <ul class="pager">
-        <li class="previous"><?php echo link_to_previous_item_show(); ?></li>
-        <li class="next"><?php echo link_to_next_item_show(); ?></li>
-    </ul>
-   
-  
 
 <?php 
 

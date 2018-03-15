49fc0e4e259c50b6e044a0eb0b8ec1ce2af4f1b9
Tim Hollies <tim.hollies@warwick.ac.uk>
2018-03-15 16:40:29 +0000

    Item pages

--- items/show.php
+++ items/show.php
@@ -11,6 +11,11 @@ else :
 
 ?>
     <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+    <?php if (metadata('item', 'has tags')): ?>
+        <div id="item-tags" class="element">
+            <div class="element-text"><?php echo tag_string('item'); ?></div>
+        </div>
+    <?php endif;?>
        
     <?php if (metadata('item',array('Item Type Metadata','Interviewee'))): ?>
         <div class="element-text">
@@ -28,7 +33,6 @@ else :
         <!-- The following returns all of the files associated with an item. -->
     <?php if (metadata('item', 'has files')): ?>
         <div id="itemfiles" class="element">
-            <h3><?php echo __('Clip'); ?></h3>
             <div class="element-text"><?php echo files_for_item(); ?></div>
         </div>
     <?php endif; ?> 
@@ -41,16 +45,11 @@ else :
         </div>
     <?php endif; ?> 
 
-        <!-- The following prints a list of all tags associated with the item -->
-        <?php if (metadata('item', 'has tags')): ?>
-        <div id="item-tags" class="element">
-            <h3><?php echo __('Tags'); ?></h3>
-            <div class="element-text"><?php echo tag_string('item'); ?></div>
-        </div>
-    <?php endif;?>
+    <!-- The following prints a list of all tags associated with the item -->
 
-    <?php echo all_element_texts('item'); ?>
-                
+    <div class="element-texts">
+        <?php echo all_element_texts('item'); ?>
+    </div>                
 
                     
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->

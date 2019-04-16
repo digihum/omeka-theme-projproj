20254f92d82b8ee525e8b56a53e54798d2b69bb7
admin <admin@ipdip.lnx.warwick.ac.uk>
2019-04-16 14:34:32 +0100

    Added cookies to remember filter selections, only items in collections now show.

--- items/browse.php
+++ items/browse.php
@@ -1,6 +1,6 @@
 <?php
 queue_js_file('lib/shuffle');
-queue_js_file('items-browse');
+queue_js_file('cookie_library');
 $pageTitle = __('Archive'); 
 echo head(array('title' => $pageTitle, 'bodyid'=>'items','bodyclass' => 'items browse'));
 
@@ -11,7 +11,7 @@ echo head(array('title' => $pageTitle, 'bodyid'=>'items','bodyclass' => 'items b
 <div class="row">
    <div class="page-header">
     <div class="col-xs-12">
-	    <h1><?php echo $pageTitle;?> <?php echo __('(%s total)', $total_results); ?></h1>
+	    <h1><?php echo $pageTitle." ";?>(<span id="total_items"></span>)
     </div>
     <nav class="col-md-6">
         <div class="items-nav navigation" id="secondary-nav">
@@ -28,37 +28,39 @@ echo head(array('title' => $pageTitle, 'bodyid'=>'items','bodyclass' => 'items b
 <div class="item hentry" >
 <div id="filter-array-decades" class="filter-array">
 
-   <span class="filter-active" data-tag-decades="all">All</span>
+   <span id="decade_span_all" class="filter-active" data-tag-decades="all">All</span>
    <?php 
        $allTags = array_map(function($tag) { return $tag['name']; }, get_records('Tag', [], 0));
        $dateTags = preg_grep("/^[0-9]{4}s$/", $allTags);
        sort($dateTags);
        foreach ($dateTags as $dateTag) {
-           echo "<span class='filter' data-tag-decades='" . $dateTag . "' >" . $dateTag . "</span>";
+           echo "<span id='decade_span_".$dateTag."' class='filter' data-tag-decades='" . $dateTag . "' >" . $dateTag . "</span>";
        }
    ?>
 </div>
 <hr/>
 Filter by Collection:
 <div id="filter-array-collections" class="filter-array">
-   <span class="filter-active" data-tag-collections="all">All</span>
+   <span id="col_span_all" class="filter-active" data-tag-collections="all">All</span>
    <?php 
 $collections = get_records('Collection',[], 1000);
 set_loop_records('collections', $collections);
 foreach (loop('collections') as $collection)
 {
     $collection_name = metadata($collection, array('Dublin Core', 'Title'));
-    echo "<span class='filter' data-tag-collections='" . $collection[id] . "' >" . $collection_name . "</span>";   
+    echo "<span id='col_span_" .$collection["id"]."' class='filter' data-tag-collections='" . $collection["id"] . "' >" . $collection_name . "</span>";   
 }
 ?>
 
 </div>
 <hr/>
-    <div class"row" id="nothing-to-see" style="visibility: hidden;">There are no items that match your filters.</div>
+    <div class="row" id="nothing-to-see" style="visibility: hidden;">There are no items that match your filters.</div>
      <div class="row masonry-layout">
-    <?php foreach (loop('items') as $item): ?>
+    <?php foreach (loop('items') as $item): 
+        if(!($item["collection_id"])) continue;
+        ?>
         <?php $tags = array_map(function($tag) { return '"' . $tag['name'] . '"'; }, $item->Tags);?>
-       <div class="masonry-item" data-decades='[<?php echo implode(', ', $tags); ?>]' data-collections='["<?php echo $item[collection_id]; ?>"]' style="min-height:200px">
+       <div class="masonry-item" data-decades='[<?php echo implode(', ', $tags); ?>]' data-collections='["<?php echo $item["collection_id"]; ?>"]' style="min-height:200px">
 
         <?php if (metadata('item', 'has thumbnail')): ?>
         <div class="item-img">
@@ -118,42 +120,56 @@ foreach (loop('collections') as $collection)
 var Shuffle = window.Shuffle;
 var element = document.querySelector('.masonry-layout');
 var sizer = element.querySelector('.sizer-element');
-var current_decade_filter = "all";
-var current_collection_filter = "all";
-
+var visible_items = 0;
+var visible_items_text = "";
 var shuffleInstance = new Shuffle(element, {
   itemSelector: '.masonry-item',
  supported: false
 });
 
+var current_decade_filter = getCookie("decade_filter");
+var current_collection_filter = getCookie("collection_filter");
+if (current_decade_filter && current_collection_filter) {shuffle(current_decade_filter,current_collection_filter);}
+else {document.getElementById("total_items").innerHTML = document.getElementsByClassName('shuffle-item--visible').length + " total";}
+
+//Add event listeners to filter buttons.
 document.querySelectorAll('.filter, .filter-active').forEach((f) => { 
-  f.addEventListener('click', (e) => {  
+  f.addEventListener('click', (e) => { 
     if(f.attributes.getNamedItem("data-tag-decades") != null) 
-    {
-        current_decade_filter = f.attributes.getNamedItem("data-tag-decades").value;
-        var active_filters = document.getElementById("filter-array-decades").getElementsByClassName('filter-active');
-        for(var i = 0; i < active_filters.length; i++) {active_filters.item(i).className = "filter";}
-        f.className = "filter-active";
-    }
-    else 
-    {
-        current_collection_filter = f.attributes.getNamedItem("data-tag-collections").value;
-        var active_filters = document.getElementById("filter-array-collections").getElementsByClassName('filter-active');
-        for(var i = 0; i < active_filters.length; i++) {active_filters.item(i).className = "filter";}
-        f.className = "filter-active";
-    }
-    shuffleInstance.filter([current_decade_filter,current_collection_filter]); 
-
-
-    if(document.getElementsByClassName('shuffle-item--visible').length == 0) 
-    {
-        document.getElementById('nothing-to-see').style.visibility = "visible";
-    }
-    else 
-    {
-        document.getElementById('nothing-to-see').style.visibility = "hidden";
-    }
+        {current_decade_filter = f.attributes.getNamedItem("data-tag-decades").value;}
+    else
+        {current_collection_filter = f.attributes.getNamedItem("data-tag-collections").value;}
+    //Store current filter selections then shuffle.
+    setCookie('decade_filter', current_decade_filter, 4);
+    setCookie('collection_filter', current_collection_filter, 4);
+    shuffle(current_decade_filter,current_collection_filter);
   });
 });
+
+//Function to shuffle filtered items and set UI text.
+function shuffle (current_decade_filter, current_collection_filter)
+{
+    //Activate the actual shuffleJS shuffle.
+    shuffleInstance.filter([current_decade_filter,current_collection_filter]); 
+    //Display number of items visible at the top of the page.
+    visible_items = document.getElementsByClassName('shuffle-item--visible').length
+    if(current_decade_filter == "all" && current_collection_filter == "all") {visible_items_text = " total";}
+    else {visible_items_text = " filtered";}
+    document.getElementById("total_items").innerHTML = visible_items + visible_items_text;
+
+    //Display "no items" message if applicable.
+    if(document.getElementsByClassName('shuffle-item--visible').length == 0) 
+        {document.getElementById('nothing-to-see').style.visibility = "visible";}
+    else 
+        {document.getElementById('nothing-to-see').style.visibility = "hidden";}
+
+    //Highlight the currently selected filter buttons.
+    var active_filters = document.getElementById("filter-array-decades").getElementsByClassName('filter-active');
+    for(var i = 0; i < active_filters.length; i++) {active_filters.item(i).className = "filter";}
+    document.getElementById("decade_span_" + current_decade_filter).className = "filter-active";
+    var active_filters = document.getElementById("filter-array-collections").getElementsByClassName('filter-active');
+    for(var i = 0; i < active_filters.length; i++) {active_filters.item(i).className = "filter";}
+    document.getElementById("col_span_" + current_collection_filter).className = "filter-active";
+}
 </script>
 <?php echo foot(); ?>
